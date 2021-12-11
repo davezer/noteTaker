5d62dae4570395f4f22c5c26ea40b3c958611174
davezer <davezer27@gmail.com>
2021-12-11 15:07:39 -0500

    restructured get and post methods to actually work

--- server.js
+++ server.js
@@ -10,27 +10,50 @@ app.use(express.urlencoded ({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-const { notes } = require('./db/db.json');
-
-
-
-
 // GET route
 app.get('/api/notes', (req, res) => {
-    res.json(notes);
+    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    });
 });
 
 // route to server to accept data to be used or stored server side
-app.post('/api/notes', (req,res) => {
-    req.body.id = notes.length.toString();
+app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = newNote.id++;
+        notes.push(newNote);
 
-    if (!validateNote(req.body)) {
-        res.status(400).send('This note is not formatted properly');
-    } else {
-        const note = creatNewNote(req.body, notes);
+        const createNote = JSON.stringify(notes);
+        fs.writeFile(path.join(__dirname, "./db/db.json"), createNote, (err) =>{
+            if (err) throw err;
+        });
+        res.json(newNote);
+    });
+});
 
-        res.json(note);
-    }
+
+// delete note
+app.delete("/api/notes/:id", function(req, res) {
+    const noteID = req.params.id;
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const notesArray = notes.filter(item => {
+            return item.id !== noteID
+        });
+        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err, data) => {
+            console.log("Deleted!")
+            if (err) throw err; 
+            res.json(notesArray) 
+
+        });
+    });
+
 });
 
 // route to index.html
@@ -38,7 +61,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 // route to notes.html
-app.get('notes', (req, res) => {
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 // console log the port is live
