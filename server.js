c1052654c64773376b7ac40d55adca4307a1d09f
davezer <davezer27@gmail.com>
2021-12-12 16:25:53 -0500

    restructured routes

--- server.js
+++ server.js
@@ -1,70 +1,20 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const express = require("express");
+const path =require("path");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.use(express.urlencoded ({ extended: true }));
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join (__dirname, "public")));
 
-// GET route
-app.get('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, './db/db.json'), (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        res.json(notes);
-    });
-});
-
-// route to server to accept data to be used or stored server side
-app.post("/api/notes", function(req, res) {
-    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        const newNote = req.body;
-        newNote.id = newNote.id++;
-        notes.push(newNote);
-
-        const createNote = JSON.stringify(notes);
-        fs.writeFile(path.join(__dirname, "./db/db.json"), createNote, (err) =>{
-            if (err) throw err;
-        });
-        res.json(newNote);
-    });
-});
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 
-// delete note
-app.delete("/api/notes/:id", function(req, res) {
-    const noteID = req.params.id;
-    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        const notesArray = notes.filter(item => {
-            return item.id !== noteID
-        });
-        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err, data) => {
-            console.log("Deleted!")
-            if (err) throw err; 
-            res.json(notesArray) 
-
-        });
-    });
-
-});
-
-// route to index.html
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-// route to notes.html
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-// console log the port is live
-app.listen(PORT, () => {
-    console.log(`Coming to you live from ${PORT}!`);
+app.listen(PORT, () => { 
+    console.log(`App listening on: ${PORT}`);
 });
