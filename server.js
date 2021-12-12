c16d493829661931b0f4ff45e49d5720b3739aca
davezer <davezer27@gmail.com>
2021-12-12 16:52:27 -0500

    reverted to a working commit, updated code to fix display issues, working delete buttons

--- server.js
+++ server.js
@@ -1,20 +1,67 @@
 const express = require("express");
-const path =require("path");
-
-const PORT = process.env.PORT || 3001;
 const app = express();
-
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+const path = require("path");
+const fs = require("fs");
+const uuid = require("uuid");
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join (__dirname, "public")));
 
-app.use("/api", apiRoutes);
-app.use("/", htmlRoutes);
+// get routes for index.html and notes.html
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+// display notes
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        res.json(notes);
+    })
+});
+
+// add new note
+app.post("/api/notes", function(req, res) {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const newNote = req.body;
+        newNote.id = uuid.v4();
+        notes.push(newNote);
+
+        const createNote = JSON.stringify(notes);
+        fs.writeFile(path.join(__dirname, "./db/db.json"), createNote, (err) =>{
+            if (err) throw err;
+        });
+        res.json(newNote);
+    });
+});
+
+// delete saved notes
+app.delete("/api/notes/:id", function(req, res) {
+    const noteID = req.params.id;
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data);
+        const notesArray = notes.filter(item => {
+            return item.id !== noteID
+        });
+        fs.writeFile('./db/db.json', JSON.stringify(notesArray), (err, data) => {
+            if (err) throw err; 
+            res.json(notesArray) 
+
+        });
+    });
+
+});
 
 
 app.listen(PORT, () => { 
-    console.log(`App listening on: ${PORT}`);
+    console.log(`Coming to you live on ${PORT}!`);
 });
